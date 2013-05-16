b914e4ae8d218de64b6bbc0a539163b93c68b30a
Brian Smith <pingwin@gmail.com>
2013-05-16 16:42:51 -0400

    * Add some formating to the README to differentiate between sections * Add example code for how to create a zone configuration

--- sdk/python/dnsdotcom_api.py
+++ sdk/python/dnsdotcom_api.py
@@ -1,41 +1,45 @@
 import httplib, urllib, json
 
 class Provision(object):
-	def __init__(self, auth_token, sandbox=False):
-		self.auth_token = str(auth_token).upper()
-		self.sandbox  = sandbox
+        def __init__(self, auth_token, sandbox=False, server=None):
+                self.auth_token = str(auth_token).upper()
+                self.sandbox  = sandbox
+                self.server   = server
 
-	def query(self, command, args={}):
-		if self.sandbox:
-			conn = httplib.HTTPConnection('sandbox.dns.com')
-		else:
-			conn = httplib.HTTPSConnection('app.dns.com')
 
-		# attach user authentication information if not already attached
-		if 'AUTH_TOKEN' not in args:
-			args['AUTH_TOKEN' ] = self.auth_token
+        def query(self, command, args={}):
+                if self.server:
+                        conn = httplib.HTTPConnection(self.server)
+                else:
+                        if self.sandbox:
+                                conn = httplib.HTTPConnection('sandbox.dns.com')
+                        else:
+                                conn = httplib.HTTPSConnection('app.dns.com')
 
-		# create query string
-		query_string = '/api/%s/?%s' % (command, urllib.urlencode(args))
-		conn.request('GET', query_string)
+                # attach user authentication information if not already attached
+                if 'AUTH_TOKEN' not in args:
+                        args['AUTH_TOKEN' ] = self.auth_token
 
-		# read response
-		resp = conn.getresponse()
-		json_string = resp.read()
-		# print json_string
-		return json.loads(json_string)
-			
+                # create query string
+                query_string = '/api/%s/?%s' % (command, urllib.urlencode(args))
+                conn.request('GET', query_string)
+
+                # read response
+                resp = conn.getresponse()
+                json_string = resp.read()
+                # print json_string
+                return json.loads(json_string)
+
 
 if __name__ == "__main__":
-	dnsObj = Provision(
-		auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-		sandbox  = False
-		)
-	result = dnsObj.query('getDomains')
+        dnsObj = Provision(
+                auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                sandbox  = False
+                )
+        result = dnsObj.query('getDomains')
 
-	if not result['meta']['success']:
-		raise Exception('Error: %s' % result['meta']['error'])
+        if not result['meta']['success']:
+                raise Exception('Error: %s' % result['meta']['error'])
 
-	import pprint
-	print pprint.pprint(result)
-	
+        import pprint
+        print pprint.pprint(result)
